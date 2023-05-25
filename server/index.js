950335a5b642c047bc2fe549d838ac97947629f7
Hanan kedir <hanankmoh@gmail.com>
2023-05-25 16:27:46 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express =require("express");
 const app= express();
 const cors = require("cors");
+const pool= require("./db");
 
 //middleware
 app.use(cors());
@@ -10,3 +11,15 @@ app.use(express.json());
 app.listen(5000, ()=>{
     console.log("server has started at port 5000")
 });
+
+//ROUTES 
+
+//create a todo
+
+//get all todos 
+
+// get a todo
+
+//update a todo
+
+//delete a todo
